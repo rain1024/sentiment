ae3561f1f49eb51f3981e695291d220502ee324a
rain <brother.rain.1024@gmail.com>
2017-08-28 11:44:44 +0700

    add eda

--- eda.py
+++ eda.py
@@ -1,4 +1,8 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
+from pylab import rcParams
 df = pd.read_excel("data/data.xlsx", encoding='sys.getfilesystemencoding()')
-print(0)
+rcParams['figure.figsize'] = 13, 6
+df.sum().plot.barh()
+plt.savefig("labels_distribution.png")
+plt.show()
