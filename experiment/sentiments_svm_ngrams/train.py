efe2b867aeaa644951f4591d653e723b20815619
Nhật Anh <buinhatanh1208@gmail.com>
2017-11-30 11:47:26 +0700

    test sentiment

--- experiment/sentiments_svm_ngrams/train.py
+++ experiment/sentiments_svm_ngrams/train.py
@@ -1,12 +1,12 @@
 from os.path import dirname, join
 from languageflow.flow import Flow
 from languageflow.model import Model
+from languageflow.model.sgd import SGDClassifier
 from languageflow.transformer.tfidf import TfidfVectorizer
 from languageflow.validation.validation import TrainTestSplitValidation
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.preprocessing import MultiLabelBinarizer
 from load_data import load_dataset
-from sklearn.linear_model import SGDClassifier
 
 if __name__ == '__main__':
     data_file = join(dirname(dirname(dirname(__file__))), "data", "fb_bank_sentiment", "corpus", "train.xlsx")
