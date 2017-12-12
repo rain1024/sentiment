4cde5b7abd128f3f1e448d6fd6870178ac51e805
rain <brother.rain.1024@gmail.com>
2017-12-12 14:22:24 +0700

    update code with update langaugeflow

--- experiment/bank_sentiment_svm/analyze.py
+++ experiment/bank_sentiment_svm/analyze.py
@@ -1,18 +1,15 @@
-import json
 from os.path import join, dirname
-from languageflow.analyze import analyze_multilabel
-from underthesea.util.file_io import write
+import languageflow
+from languageflow.log import MultilabelLogger
 from load_data import load_dataset
 from model import sentiment
 
-data_file = join(dirname(dirname(dirname(__file__))), "data", "fb_bank_sentiment",
-                 "corpus", "test.xlsx")
+data_file = join(dirname(dirname(dirname(__file__))), "data",
+                 "fb_bank_sentiment", "corpus", "test.xlsx")
 X_test, y_test = load_dataset(data_file)
 y_test = [tuple(item) for item in y_test]
 y_pred = sentiment(X_test)
 
-result = analyze_multilabel(X_test, y_test, y_pred)
-
-content = json.dumps(result, ensure_ascii=False)
-write("analyze/result.json", content)
-print("F1 Weighted:", result["f1_weighted"])
+log_folder = join(dirname(__file__), "analyze")
+MultilabelLogger.log(X_test, y_test, y_pred, folder=log_folder)
+languageflow.board(log_folder)
