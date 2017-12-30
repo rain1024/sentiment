6f76a6b91aa63c638d7680897ad03b353d0966bf
jackNhat <buinhatanh1208@gmail.com>
2017-12-30 11:36:19 +0700

    turning logistic regression model with 2200 sentiments

---
+++ experiment/lr_count13_maxdf08_mindf8_maxfeature5k/test_model.py
@@ -0,0 +1,23 @@
+from model import sentiment
+from unittest import TestCase
+
+
+class TestSentiment(TestCase):
+    def test_sentiment(self):
+        text = "Gọi mấy lần mà lúc nào cũng là các chuyên viên đang bận hết ạ "
+        actual = sentiment(text)
+        expected = "CUSTOMER SUPPORT#NEGATIVE"
+        self.assertEquals(actual[0], expected)
+
+    def test_sentiment_1(self):
+        text = "Chúc mừng VCB, luôn thành công và tạo niềm tin cho khách hàng an tâm khi đồng hành cùng VCB."
+        actual = sentiment(text)
+        expected = "TRADEMARK#POSITIVE"
+        self.assertEquals(actual[0], expected)
+
+    def test_sentiment_2(self):
+        text = "Bạn ra ngân hàng hỏi luôn cho nhanh giải đáp qua đây ko ăn thua lắm."
+        actual = sentiment(text)
+        expected = "CUSTOMER SUPPORT#NEGATIVE"
+        self.assertEquals(actual[0], expected)
+
